4468c33610157ab441fd8d6391a5ad53812eaa92
Raul Sebastian Carreno Reyes <raul.carreno@wom.co>
2024-11-27 14:47:50 -0500

    :bug: error solutions

--- main.tf
+++ main.tf
@@ -53,12 +53,12 @@ resource "aws_api_gateway_rest_api" "lambda_api" {
 # Crear un recurso de integración Lambda en API Gateway
 resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
-  resource_id = aws_api_gateway_rest_api.ambda_api.root_resource_id
+  resource_id = aws_api_gateway_rest_api.lambda_api.root_resource_id
   http_method = aws_api_gateway_method.lambda_method.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.lambda_function.invoke_arn
+  uri                     = aws_lambda_function.lab-lambda.invoke_arn
 }
 
 # Crear un método para manejar las peticiones en la API
@@ -76,7 +76,7 @@ resource "aws_lambda_permission" "api_gw_invoke_permission" {
   function_name = aws_lambda_function.lab-lambda.function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_api_gateway_rest_api.lambda-api.execution_arn}/*/*"
+  source_arn = "${aws_api_gateway_rest_api.lambda_api.execution_arn}/*/*"
 }
 # Crear una implementación de la API
 resource "aws_api_gateway_deployment" "deployment" {
